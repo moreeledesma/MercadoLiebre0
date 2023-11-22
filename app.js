2e65f0b526275f015a577c9fa6f65066daa86e7d
moreeledesma <morea.ledesma111@gmail.com>
2023-11-21 21:03:55 -0300

    Borrando la carpeta NodeModules

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const publicPath = path.resolve("public");
+const PORT = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
 
@@ -9,6 +10,15 @@ app.get("/", (req,res)=>{
     res.sendFile(path.resolve("./views/home.html"));
 });
 
-app.listen(3000, ()=>{
-    console.log("Servidor corriendo");
+
+app.get("/register", (req,res)=>{
+    res.sendFile(path.resolve("./views/register.html"));
 });
+
+app.get("/login", (req,res)=>{
+    res.sendFile(path.resolve("./views/login.html"));
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor Funcionando en el puerto ${PORT}`);
+})
