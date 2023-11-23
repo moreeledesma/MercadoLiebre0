bb221fcf0ff84c3e335f767db0b5b96b1c8d49a6
moreeledesma <morea.ledesma111@gmail.com>
2023-11-23 19:44:28 -0300

    Modificaciones

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const publicPath = path.resolve("public");
+//Seleccion de la varaibale de entorno
 const PORT = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
